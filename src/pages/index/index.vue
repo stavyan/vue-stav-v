3c8a85ce5b13fc7e0300c3e20681240415b6eb19
stavyan <617946852@qq.com>
2019-01-06 23:05:48 +0800

    小程序初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,7 @@
 <template>
   <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-  </div>
+    <canvas canvas-id='firstCanvas' style='position:absolute;z-index:25;width:100%;height:100%;'></canvas>
+ </div>
 </template>
 
 <script>
